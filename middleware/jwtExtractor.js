6d61151b7f5636f9442bca4524aa2394b84d93f6
Mr. Prashanth Chowdary <ch.prashanthchowdary007@gmail.com>
2025-02-11 16:06:05 +0530

    completed cart routes

---
+++ middleware/jwtExtractor.js
@@ -0,0 +1,10 @@
+const jwtExtraction = (request,response,next)=>{
+    const authentication = request.get('authorization');
+    if(authentication && authentication.startsWith('Bearer ')){
+        const token = authentication.replace('Bearer ','')
+        request.token = token
+    }
+    next()
+}
+
+module.exports = jwtExtraction
---
+++ middleware/userExtractor.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const key = process.env.JWT_SECRET
+const userExtractor = (request,response,next)=>{
+    try{
+        const user = jwt.verify(request.token,key)
+        request.id = user.id
+        request.username = user.username
+    }catch(e){
+        console.error(e)
+    }
+    next()
+}
+
+module.exports = userExtractor
