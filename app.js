f7432f79168a9481acd289b6460a54ec80a231b7
Mr. Prashanth Chowdary <ch.prashanthchowdary007@gmail.com>
2025-02-20 14:48:54 +0530

    cors fix in production

--- app.js
+++ app.js
@@ -24,13 +24,15 @@ mongoose.connect(config.MONGODB_URI).then(()=>{
     console.log('connection error',error)
 })
 
+
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload());
+
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
-app.use(cors())
-app.use(express.json())
-app.use(fileUpload());
 app.use('/api/products',productRoute)
 app.use('/api/auth',authRoute)
 app.use(jwtExtraction)
